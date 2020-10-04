07bde4f8ac787fb8a72907cc6e022fe61c926153
Shaurya Dewan <shaurya.dewan@students.iiit.ac.in>
2020-10-04 15:37:19 +0530

    Added 'add patient' functionality.

--- main.py
+++ main.py
@@ -17,6 +17,88 @@ def qexec(query):
         print("Could not insert into database! Error pymysql %d: %s" %(e.args[0], e.args[1]))
         return -1;
 
+def addins(patid):
+
+    tmp = sp.call('clear', shell = True)
+    inspat = {}
+    insdet = {}
+    
+    inspat["Patient_Id"] = patid
+    inspat["Insurance_Id"] = int(input("Insurance Id: "))
+
+    insdet["Insurance_Id"] = inspat["Insurance_Id"]
+    insdet["Company"] = (input("Company: "))
+    insdet["Latest_Renewal_Date"] = input("Latest Renewal Date (YYYY-MM-DD): ")
+    query = """insert into Insured_Details values(%d, "%s", "%s")""" % (insdet["Insurance_Id"], insdet["Company"], insdet["Latest_Renewal_Date"])
+    if(qexec(query)):
+        return -1;
+
+    query = """insert into Insured_Patients values(%d, %d)""" % (inspat["Patient_Id"], inspat["Insurance_Id"])
+    if(qexec(query)):
+        return -1;
+
+    return 0
+
+def addpat():
+
+    try:
+        tmp = sp.call('clear', shell = True)
+        patient = {}
+
+        flag = 1
+        query = "select Patient_Id from Patient"
+        if(qexec(query)):
+            return -1;
+        ids = cur.fetchall()
+
+        while(flag):
+            temp = random.randint(0, inf)
+            patient["Patient_Id"] = temp
+            flag = 0
+            for oid in ids:
+                if(oid == temp):
+                    flag = 1
+                    break
+
+        print("Enter new patient's details: ")
+        name = (input("Name (Fname Lname): ")).split(' ')
+        patient["First_Name"] = name[0]
+        patient["Last_Name"] = name[1]
+        patient["Contact_No"] = int(input("Contact Number(10 digit): "))
+        patient["Date_of_Birth"] = (input("Birth Date (YYYY-MM-DD): "))
+
+        hno = (input("*House Number: "))
+        if(hno == ""):
+            patient["H_No"] = "NULL"
+        else:
+            patient["H_No"] = int(hno)
+        
+        patient["Street"] = (input("*Street: "))
+        zip = (input("*Zipcode: "))
+        if(zip == ""):
+            patient["Zipcode"] = "NULL"
+        else:
+            patient["Zipcode"] = int(zip)
+        
+        patient["City"] = (input("*City: "))
+
+        query = """insert into Patient values(%d, "%s", "%s", %s, "%s", "%s", %s, %d, "%s")""" % (patient["Patient_Id"], patient["First_Name"], patient["Last_Name"], patient["H_No"], patient["Street"], patient["City"], patient["Zipcode"], patient["Contact_No"], patient["Date_of_Birth"])
+        if(qexec(query)):
+            return -1
+
+        insure = input("Is the patient insured(y / n): ")
+        if(insure == "y"):
+            if(addins(patient["Patient_Id"])):
+                return -1
+
+        tmp = input("Enter any key to CONTINUE:")
+        return 0
+
+    except Exception as e:
+        con.rollback()
+        print("Error: ", e)
+        return -1
+
 def adddept(dno, staff_id):
         
     tmp = sp.call('clear', shell = True)
@@ -142,7 +224,7 @@ def addstaff():
         staff["Last_Name"] = name[1]
         staff["Sex"] = input("Sex (F/M): ")
         staff["Salary"] = int(input("Salary: "))
-        staff["Contact_No"] = (input("Contact Number(10 digit): "))
+        staff["Contact_No"] = int(input("Contact Number(10 digit): "))
         staff["Date_of_Birth"] = (input("Birth Date (YYYY-MM-DD): "))
 
         hno = (input("*House Number: "))
@@ -166,7 +248,7 @@ def addstaff():
         else:
             staff["Supervisor_Id"] = "NULL"
 
-        query = """insert into Staff values(%d, "%s", "%s", "%s", %d, "%s", "%s", %s, "%s", %s, "%s", "%s", %s)""" % (staff["Staff_Id"], staff["First_Name"], staff["Last_Name"], staff["Sex"], staff["Salary"], staff["Contact_No"], staff["Date_of_Birth"], staff["H_No"], staff["Street"], staff["Zipcode"], staff["City"], staff["Job"], staff["Supervisor_Id"])
+        query = """insert into Staff values(%d, "%s", "%s", "%s", %d, %s, "%s", %s, "%s", %s, "%s", "%s", %s)""" % (staff["Staff_Id"], staff["First_Name"], staff["Last_Name"], staff["Sex"], staff["Salary"], staff["Contact_No"], staff["Date_of_Birth"], staff["H_No"], staff["Street"], staff["Zipcode"], staff["City"], staff["Job"], staff["Supervisor_Id"])
         if(qexec(query)):
             return -1;
 
@@ -198,7 +280,13 @@ def dispatch(ch):
             return 0
 
     elif(ch == 2):
-        addpat()
+        if(addpat()):
+            con.rollback
+            return -1
+        else:
+            con.commit()
+            return 0
+
     elif(ch == 3):
         option3()
     elif(ch == 4):
