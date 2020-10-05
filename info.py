48ec20ee33a72b28d882bf77729bf08f2a92c42f
Debayan <sahadebayan28@gamil.com>
2020-10-05 16:53:55 +0530

    made the necessary changes in the info.py file

--- info.py
+++ info.py
@@ -13,6 +13,7 @@ def info(opti):
                 return -1
 
             res = cur.fetchall()
+            print("Room_No   Location_Block   Location_Floor   Room_Type");
             for row in res:
                 print(
                     row['Room_No'],
@@ -22,47 +23,51 @@ def info(opti):
 
         elif(opti == 2):
             shift = input("Shift Day: ")
-            query = """select First_Name Last_Name from Staff inner join Shift on Staff.Staff_Id= Shift.Staff_Id where Shift_Day = "%s" """ % (
+            query = """select First_Name, Last_Name from Staff inner join Shift on Staff.Staff_Id= Shift.Staff_Id where Shift_Day = "%s" """ % (
                 shift)
             if(qexec(query)):
                 return -1
 
             res = cur.fetchall()
+            print("First_Name   Last_Name");
             for row in res:
                 print(row['First_Name'], row['Last_Name'])
 
         elif(opti == 3):
             spec = input("Specialisation: ")
-            query = """select First_Name Last_Name from Staff inner join Specialisation on Staff.Staff_Id= Specialisation.Staff_Id where Expertise_Area = "%s" """ % (
+            query = """select First_Name, Last_Name from Staff inner join Specialisation on Staff.Staff_Id= Specialisation.Staff_Id where Expertise_Area = "%s" """ % (
                 spec)
             if(qexec(query)):
                 return -1
 
             res = cur.fetchall()
+            print("First_Name   Last_Name");
             for row in res:
                 print(row['First_Name'], row['Last_Name'])
 
         elif(opti == 4):
             comp = input("Companyname: ")
-            query = """select First_Name Last_Name from Patient inner join Insured_Details on Patient.Insurance_Id= Insured_Details.Insurance_Id where Company = "%s" """ % (
+            query = """select First_Name, Last_Name from Patient inner join Insured_Details on Patient.Insurance_Id= Insured_Details.Insurance_Id where Company = "%s" """ % (
                 comp)
             if(qexec(query)):
                 return -1
 
             res = cur.fetchall()
+            print("First_Name   Last_Name");
             for row in res:
                 print(row['First_Name'], row['Last_Name'])
 
         elif(opti == 5):
             patno = int(input("Patient_Id: "))
-            query = "select Patient_Id, Contact_No from Patient where Patient_Id = %d " % (
+            query = "select Patient_Id, First_Name, Last_Name, Contact_No from Patient where Patient_Id = %d " % (
                 patno)
             if(qexec(query)):
                 return -1
 
             res = cur.fetchall()
+            print("Patient_Id   First_Name   Last_Name   Contact_No");
             for row in res:
-                print(row['Patient_Id'], row['Contact_No'])
+                print(row['Patient_Id'], row['First_Name'], row['Last_Name'], row['Contact_No'])
 
         elif(opti == 6):
             pno = int(input("Pno: "))
@@ -71,6 +76,7 @@ def info(opti):
                 return -1
 
             res = cur.fetchall()
+            print("Pno   Complaint   Diagnosis");
             for row in res:
                 print(row['Pno'], row['Complaint'], row['Diagnosis'])
 
@@ -81,6 +87,7 @@ def info(opti):
                 return -1
 
             res = cur.fetchall()
+            print("Amount");
             for row in res:
                 print(row['Amount'])
 
@@ -91,6 +98,7 @@ def info(opti):
                 return -1
 
             res = cur.fetchall()
+            print("DNo   Dname   Location_Block   Location_Floor");
             for row in res:
                 print(
                     row['DNo'],
@@ -100,12 +108,13 @@ def info(opti):
 
         elif(opti == 9):
             medname = input("Medicine Name: ")
-            query = """select Batch_No Qty from Batch_Details inner join Medication on Batch_Details.Batch_No= Medication.Batch_No where Med_Name = "%s" """ % (
+            query = """select Batch_No, Qty from Batch_Details inner join Medication on Batch_Details.Batch_No= Medication.Batch_No where Med_Name = "%s" """ % (
                 medname)
             if(qexec(query)):
                 return -1
 
             res = cur.fetchall()
+            print("Batch_No   Qty");
             for row in res:
                 print(row['Batch_No'], row['Qty'])
 
